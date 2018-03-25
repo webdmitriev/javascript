8f5cc122a794b0bfff488e89d3972315fb9875a1
Oleg <webdmitriev@gmail.com>
2018-03-25 20:51:59 +0300

    Это наш первый комит

---
+++ lesson1/js/script.js
@@ -0,0 +1,23 @@
+var myMoney 	= prompt('Ваш бюджет? '),
+	myMagazin 	= prompt('Название вашего магазина: '),
+	score1 		= prompt('Какой тип товара будем продавать?'),
+	score2 		= prompt('А ещё какой?'),
+	score3 		= prompt('Давай ещё что-нибудь?'),
+	employees	= prompt('Сколько сотрудников в штате?'),
+	mainList 	= {
+		money: myMoney,
+		price: myMagazin,
+		shopGoods: [score1, score2, score3],
+		employers: {employees},
+		open: false
+	};
+
+var year = prompt('Ваня, с какого я потока Веб-разработчик?', '');
+if (year == 9) {
+  alert( 'Красавчик!' );
+} else {
+  alert( 'А вот и неправильно!' ); // любое значение, кроме 9
+};
+
+console.log(mainList);
+console.log(myMoney/30);
